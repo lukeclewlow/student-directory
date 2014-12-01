691da2920e9faa74cfd485d320eef3f419e475db
Luke Clewlow <luke.clewlow@gmail.com>
2014-12-01 17:42:36 +0000

    Exercise 4

--- directory.rb
+++ directory.rb
@@ -24,7 +24,7 @@ end
 
 def print(students)
 	students.each_with_index do | student, index |
-		puts "#{index + 1}: #{student[:name]} (#{student[:cohort]} cohort)" if student[:name].start_with? "A"
+		puts "#{index + 1}: #{student[:name]} (#{student[:cohort]} cohort)" if student[:name].length >= 12
 	end
 end
 
