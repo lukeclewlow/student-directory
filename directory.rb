0ca2eafd49e60583b03c0487dd88cc2368c3bf5b
Luke Clewlow <luke.clewlow@gmail.com>
2014-12-01 14:04:15 +0000

    updated directory to add summary

--- directory.rb
+++ directory.rb
@@ -1,4 +1,5 @@
 puts ""
+#first we print the list of students
 puts "The students of my cohort at Makers Academy"
 puts "------------"
 puts "Mr. Pink"
@@ -7,3 +8,6 @@ puts "Mr. Orange"
 puts "Mr. Blue"
 puts "Mr. Red"
 
+print "Overall, we have "
+print 5
+puts "great students."
