ba59a3d94c117d5f5a5aaaebc5dfc5310f20ae82
Luke Clewlow <luke.clewlow@gmail.com>
2014-12-01 14:53:25 +0000

    Added methods

--- directory.rb
+++ directory.rb
@@ -7,11 +7,24 @@ students = [
 "Mrs. Red"
 ]
 
-#Now we iterate over each student and puts their name to the screens
-puts "The students of my cohort at Makers Academy"
-puts "-------------"
-students.each { |student| puts student }
+def print_header
+	puts "The students of my cohort at Makers Academy"
+	puts "-------------"
+end
 
-#finally we print the total
-puts "Overall we have #{students.length} great students!"
+def print(names)
+	names.each do |name|
+		puts name
+	end
+end
+
+def print_footer(names)
+	puts "Overall, we have #{names.length} great students."
+end
+
+#nothing happens until we call the methods
+
+print_header
+print(students)
+print_footer(students)
 
